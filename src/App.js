b0123e5e2615ba4863c8add5c7c8cf1c84965daf
arnaud <arnaud_lafon@hotmail.fr>
2018-11-12 10:59:44 +0100

    add stat modal

--- src/App.js
+++ src/App.js
@@ -8,20 +8,41 @@ import Alert from "./components/general/Alert";
 import PrivateRoute from "./components/PrivateRoute";
 import {authenticate} from "./helpers/authentication";
 import store from "./store";
+import Stat from "./components/stat/Stat";
+import Modal from 'react-responsive-modal';
 
 class App extends Component {
 
     constructor(props) {
         super(props);
 
+        this.state = {
+            open: false
+        };
+
         authenticate(props.location);
     }
 
+    onOpenModal = () => {
+        this.setState({ open: true });
+    };
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    };
+
     render() {
+        const { open } = this.state;
         return (
             <div>
                 {/* TODO: remove it, just an indicator for now */}
                 {store.getState().userReducer.token ? 'CONNECTED' : ''}
+                <button onClick={this.onOpenModal}>Open modal</button>
+
+                <Modal open={ open } onClose={ this.onCloseModal } center>
+                    <Stat />
+                </Modal>
+
                 <Alert />
                 <Route path="/(login|signup)" component={ Auth } />
                 <PrivateRoute path="/(|profile)" component={ Menu } />
---
+++ src/components/stat/Stat.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Stat extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Statistiques "#player"</h2>
+                <div className="stat-content-container">
+                    <div className="col col-50">Nombre de parties jouées</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+                <div className="stat-content-container">
+                    <div className="col col-50">Nombre de parties gagnées</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+                <div className="stat-content-container">
+                    <div className="col col-50">Nombres de manches jouées</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+                <div className="stat-content-container">
+                    <div className="col col-50"> Nombre de manches gagnées</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+                <div className="stat-content-container">
+                    <div className="col col-50">Argent gagné total</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+                <div className="stat-content-container">
+                    <div className="col col-50">Plus gros pot remporté</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+                <div className="stat-content-container">
+                    <div className="col col-50">Meilleur combinaison</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+                <div className="stat-content-container">
+                    <div className="col col-50">Informations du joueur</div>
+                    <div className="col col-50 txt-right">qsdqsd</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Stat;
