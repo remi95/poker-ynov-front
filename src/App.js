d45744176207863633e9aa8ac9066d2eea68d91a
arnaud <arnaud_lafon@hotmail.fr>
2018-11-13 10:14:53 +0100

    add node sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
-import './App.css';
 import Auth from "./components/authentication/Auth";
 import Menu from "./components/home/Menu";
 import Game from "./components/game/Game";
