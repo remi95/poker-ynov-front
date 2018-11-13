707e4671d9b4708e7d4c3b9c4bff714cce0cf39c
remi95 <remimafat@gmail.com>
2018-11-13 09:33:44 +0100

    Remove duplicate import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
 import './App.css';
-import { withRouter } from 'react-router-dom'
 import Auth from "./components/authentication/Auth";
 import Menu from "./components/home/Menu";
 import Game from "./components/game/Game";
